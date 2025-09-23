7ab266f71e3cef6d1440ab59dd4e26b4b4b5d5bc
ReyhaneMahmoudzade <rs.mahmoudzadeh@gmail.com>
2025-09-23 21:57:51 +0330

    Update AddDatacenter.js

--- src/pages/addDatacenter/AddDatacenter.js
+++ src/pages/addDatacenter/AddDatacenter.js
@@ -1,18 +1,47 @@
+import { useState } from 'react';
 import './AddDatacenter.css'
 import TextField from '@mui/material/TextField';
 
 
 export default function AddDatacenter() {
+
+  const [title, setTitle] = useState('')
+  const [code, setCode] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const registerHandler = event => {
+    event.preventDefault()
+
+    let datacenterInfo = {
+      title,
+      code,
+      desc
+    }
+
+    fetch('https://dcim-bb92a-default-rtdb.firebaseio.com/datacenter.json', {
+      method: 'POST',
+      body: JSON.stringify(datacenterInfo)
+    }).then(response => console.log(response))
+  }
+
+
+
   return (
     <>
-      <form action="">
-        <h2 style={{ display: 'block' }}>Add New Datacenter</h2>
+      <form onSubmit={registerHandler}>
+        <h2>Add New Datacenter</h2>
         <br />
-        <TextField id="standard-basic" label="Title" variant="standard" />
+        <TextField id="Title" label="Title" variant="standard" value={title} onChange={(event) => setTitle(event.target.value)} />
         <br />
-        <TextField id="standard-basic" label="Unique Code" variant="standard" />
+        <TextField id="Unique Code" label="Unique Code" variant="standard" value={code} 
+        onChange={(event) => setCode(event.target.value)} />
         <br />
-        <TextField id="standard-basic" label="Description" variant="standard" />
+        <TextField id="Description" label="Description" variant="standard" value={desc} 
+        onChange={(event) => setDesc(event.target.value)} />
+
+        <button className="form-field" type="submit">
+          Save
+        </button>
 
       </form>
 
